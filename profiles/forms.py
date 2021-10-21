83e312ac5b617066c0c58113f0cf79cb2eb8b14c
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-10-21 18:12:43 -0400

    Added profile information to profile screen.

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import UserProfile
-
+from django.contrib.auth.models import User
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
@@ -30,5 +30,33 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = ('password', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'username': 'Username',
+            'email': 'Email',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
             self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UserForm
+from django.contrib.auth.models import User
 
 from checkout.models import Order
 
@@ -11,21 +12,27 @@ from checkout.models import Order
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+    user = get_object_or_404(User, username=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
+        form_two = UserForm(request.POST, instance=user)
+        if form.is_valid() and form_two.is_valid():
             form.save()
+            form_two.save()
             messages.success(request, 'Profile updated successfully')
         else:
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
+        form_two = UserForm(instance=user)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
+        'page': 'profile',
         'form': form,
+        'form_two': form_two,
         'orders': orders,
         'on_profile_page': True
     }
@@ -43,6 +50,7 @@ def order_history(request, order_number):
 
     template = 'checkout/checkout_success.html'
     context = {
+        'page': 'profile',
         'order': order,
         'from_profile': True,
     }
