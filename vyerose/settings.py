3f4aac51711cbc5a84515e33e21e6e6b84bf99dc
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-11-02 12:41:54 -0400

    Got cart to function as expected.

--- vyerose/settings.py
+++ vyerose/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'products',
     'profiles',
     'checkout',
+    'cart',
 ]
 
 MIDDLEWARE = [
--- vyerose/urls.py
+++ vyerose/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('profiles/', include('profiles.urls')),
     path('accounts/', include('allauth.urls')),
+    path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
