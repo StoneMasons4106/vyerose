aadc273889378f22ff9f4d68b829e77dace21d8a
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-11-12 00:10:54 -0500

    FIxed footer position js.

--- static/js/footer_position.js
+++ static/js/footer_position.js
@@ -1,8 +1,16 @@
-$(window).resize(function(){
+function footerPosition() {
     var windowHeight = $(window).height();
     var documentHeight = $(document).height();
         
     if (documentHeight == windowHeight) {
-        $("#footer").css({'position': 'fixed', 'bottom': '0px', 'width': '100%'})
+        $("#footer").css({'position': 'fixed', 'bottom': '0px', 'width': '100%'});
     }
+}
+
+$(document).ready(function() {
+    footerPosition();
+})
+
+$(window).resize(function() {
+    footerPosition();
 })
