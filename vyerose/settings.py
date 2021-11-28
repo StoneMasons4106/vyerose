6e05db0d8630f8be6a45a779410088f6bbc283d7
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-11-28 10:48:19 -0500

    Enforce HTTPS.

--- vyerose/settings.py
+++ vyerose/settings.py
@@ -32,8 +32,11 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1', os.environ.get("HEROKU_HOSTNAME")]
+    SECURE_SSL_REDIRECT = False
 else:
     ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
+    SECURE_SSL_REDIRECT = True
+
 
 # Application definition
 
