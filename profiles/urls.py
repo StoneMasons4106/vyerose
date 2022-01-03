d3b9ce7d85de1f5508057a18131f0b3246dce35c
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-01-03 01:50:24 -0500

    Capability to delete profiles if one so chooses.

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('me', views.profile, name='me'),
     path('edit_profile', views.edit_profile, name='edit_profile'),
+    path('delete_profile', views.delete_profile, name='delete_profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
@@ -75,6 +75,27 @@ def edit_profile(request):
         'form': form,
         'form_two': form_two,
         'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_profile(request):
+    '''Delete user profile'''
+    if request.method == "POST":
+    
+        user = get_object_or_404(User, username=request.user)
+        user.delete()
+
+        messages.success(request, 'You have successfully deleted your profile.')
+        return redirect(reverse('home'))
+    
+    template = "profiles/delete_profile.html"
+
+    context= {
+        'page':'profile',
         'on_profile_page': True,
     }
 
